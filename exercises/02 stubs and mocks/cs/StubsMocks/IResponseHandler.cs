aeaf6165081214027dabb83b3f73b949f43e4166
Tonni Tielens <tonnitielens@gmail.com>
2016-07-08 16:02:20 +0200

    Add C# code for stubs and mocks exercise

---
+++ exercises/02 stubs and mocks/cs/StubsMocks/IResponseHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StubsMocks
+{
+    interface IResponseHandler
+    {
+        void Receive(string message);
+        string Name { get; }
+    }
+}
---
+++ exercises/02 stubs and mocks/cs/StubsMocks/Notifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StubsMocks
+{
+    class Notifier
+    {
+        private IList<IResponseHandler> responseHandlers = new List<IResponseHandler>();
+
+        public void Add(IResponseHandler subscriber)
+        {
+            responseHandlers.Add(subscriber);
+        }
+
+        public void Publish(string message)
+        {
+            foreach (var handler in responseHandlers)
+            {
+                Console.WriteLine($"Notifying {handler.Name} with message: {message}");
+                handler.Receive(message);
+            }
+        }
+    }
+}

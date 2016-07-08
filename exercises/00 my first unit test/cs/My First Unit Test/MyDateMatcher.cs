6074825c2df9a89a1087effd832c7f798cc02361
Tonni Tielens <tonnitielens@gmail.com>
2016-07-08 15:07:47 +0200

    Create CS version of exercise 0

---
+++ exercises/00 my first unit test/cs/My First Unit Test/MyDateMatcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_First_Unit_Test
+{
+    public class MyDateMatcher
+    {
+        private readonly IList<DateTime> myDates;
+
+        public MyDateMatcher(IList<DateTime> dates)
+        {
+            myDates = dates;
+        }
+
+        public IList<DateTime> DatesAreMatching(MyFuzzyDate fuzzyDate)
+        {
+            var matches = myDates.Where(fuzzyDate.Contains).ToList();
+
+            return matches;
+        }
+    }
+}

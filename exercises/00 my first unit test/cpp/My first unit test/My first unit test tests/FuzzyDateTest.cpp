5a2ba2751d87c333b7916e41ee1777bc9fe148d3
Tonni Tielens <tonnitielens@gmail.com>
2016-11-04 21:13:15 +0100

    First working example with Google Mock

--- exercises/00 my first unit test/cpp/My first unit test/My first unit test tests/FuzzyDateTest.cpp
+++ exercises/00 my first unit test/cpp/My first unit test/My first unit test tests/FuzzyDateTest.cpp
@@ -1,5 +1,6 @@
 #include "FuzzyDate.h"
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
 class FuzzyDateTest : public ::testing::Test
 {
@@ -18,4 +19,31 @@ protected:
 TEST_F(FuzzyDateTest, testContains)
 {
 	EXPECT_TRUE(true);
+}
+
+class IFoo
+{
+public:
+	virtual ~IFoo() = 0 {}
+
+	virtual int getBar() const = 0;
+};
+
+class FooMock : public IFoo
+{
+public:
+	MOCK_CONST_METHOD0(getBar, int());
+
+};
+
+TEST(MockTest, testMock)
+{
+	using ::testing::Return;
+	using ::testing::Exactly;
+	FooMock fooMock;
+	ON_CALL(fooMock, getBar()).WillByDefault(Return(4));
+	EXPECT_CALL(fooMock, getBar()).Times(Exactly(1));
+
+	auto bar = fooMock.getBar();
+	EXPECT_EQ(4, bar);
 }

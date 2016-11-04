c248776865bfa40903ffad6c850aa42daf3b25fa
Tonni Tielens <tonnitielens@gmail.com>
2016-11-04 20:47:58 +0100

    Write first working version of unit test using gtest

---
+++ exercises/00 my first unit test/cpp/My first unit test/My first unit test tests/FuzzyDateTest.cpp
@@ -0,0 +1,21 @@
+#include "FuzzyDate.h"
+#include "gtest/gtest.h"
+
+class FuzzyDateTest : public ::testing::Test
+{
+protected:
+	virtual void SetUp()
+	{
+
+	}
+
+	virtual void TearDown()
+	{
+
+	}
+};
+
+TEST_F(FuzzyDateTest, testContains)
+{
+	EXPECT_TRUE(true);
+}
